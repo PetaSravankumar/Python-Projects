48df70dc7e1b352e7e9022f0428c3c45efb918c6
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-10-23 22:13:07 +0530

    Added local project files

--- bank.py
+++ bank.py
@@ -1,2 +1,6 @@
 Name=input("enter the Name of the customer:")
 Account_Number=int(input("enter the account Number:"))
+Balance=1000
+print("Customer Name :",Name)
+print("Account Number :",Account_Number)
+print("Balance Amount :",Balance)
