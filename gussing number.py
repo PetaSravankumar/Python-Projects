7a1218a95465ade3e44243325e40b28fa45f2c41
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-11-21 19:33:07 +0530

    Added local project files

--- gussing number.py
+++ gussing number.py
@@ -6,18 +6,21 @@ def no_of_chances():
         print("you have 2 chances !")
     elif count==1:
         print("you have 1 chance !")
-    else:
-        print("you have Loss try again")
-        print("Number is ",n)
+        for i in range(2,12):
+            if n%i==0:
+                
 while count<=2:
     guss=int(input("guss the number :"))
     if guss==n:
         print("Conguralations")
         print("You have gussed it correct ",n)
     elif n>guss :
-        print("The gussing number is too low ")
+        print("The guess a number is greater ",guss)
         no_of_chances()
     elif n<guss:
-        print("The gussing is high")
+        print("The guess a number is less than ",guss)
         no_of_chances()
     count+=1
+if count==3 and guss!=n:
+    print("you have Loss try again")
+    print("Number is ",n)
