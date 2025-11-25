8a8256a7699ccc9a90f5710a215f4557b841452b
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-11-25 11:46:58 +0530

    Added local project files

--- rock paper.py
+++ rock paper.py
@@ -20,22 +20,31 @@ elif num==3:
     choice="Paper"
 if choice=="Rock":
     if "Rock"==fun():
+        print("You choose :Rock")
         print("It is draw ")
     elif "Scissor"==fun():
+        print("You choose :Rock")
         print("You win")
     else:
+        print("You choose :Rock")
         print("You loss")
 elif choice=="Scissor":
     if "Rock"==fun():
+        print("You choose :Scissor")
         print("It is Loss ")
     elif "Scissor"==fun():
+        print("You choose : SCissor")
         print("You Draw")
     else:
+        print("You choose : Scissor ")
         print("You Win")
 else:
     if "Rock"==fun():
+        print("You choose :Paper")
         print("It is Win")
     elif "Scissor"==fun():
+        print("You choose :Paper")
         print("You loss")
     else:
+        print("You choose :Paper")
         print("You Draw")
