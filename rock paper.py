ab343ee0cbef7a12c13b93bad4d7da04aef310f8
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-11-25 11:36:47 +0530

    Added local project files

--- rock paper.py
+++ rock paper.py
@@ -1,8 +1,20 @@
 import random
 n=random.randint(1,3)
-if n==1:
-    print("Rock")
-elif n==2:
-    print("Scissor")
-else:
-    print("Paper")
+def fun():
+    if n==1:
+        print("computer choose :" ,"Rock")
+        return "Rock"
+    elif n==2:
+        print("computer choose :" ,"Scissor")
+        return "Scissor"
+    else:
+        print("computer choose :" ,"Paper")
+        return "Paper"
+num=int(input("Enter the number to Choose the 1: Rock , 2: Scissor ,3 : Paper"))
+choice=""
+if num==1:
+    choice="Rock"
+if "Rock"==fun():
+    print("It is draw ")
+elif "Scissor"==fun():
+    print("")
